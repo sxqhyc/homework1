e71de736838fa58ad06c47c9d81aa01a7eec8b62
sxqhyc <3020272301@qq.com>
2024-08-20 14:03:36 +0800

    second commit login

--- backend/demo/src/main/java/com/example/demo/controller/UserController.java
+++ backend/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -19,6 +19,15 @@ public class UserController {
     @Resource
     UserMapper userMapper;
 
+    @PostMapping("/login")
+    public Result<?> login(@RequestBody User user){
+        User res = userMapper.selectOne(Wrappers.<User>lambdaQuery().eq(User::getUsername,user.getUsername()).eq(User::getPassword, user.getPassword()));
+        if(res == null){
+            return Result.error("-1","用户名或密码错误");
+        }
+        return Result.success();
+    }
+
     @PostMapping
     public Result<?> save(@RequestBody User user){
         if(user.getPassword() == null){
